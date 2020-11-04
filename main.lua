52e288a695f4803d036d8f61d40306c6b52207db
Petr Veselý <petrveslo55@gmail.com>
2020-11-04 18:53:16 +0100

    LÖVE 11.3 update

--- main.lua
+++ main.lua
@@ -8,60 +8,45 @@ end
 
 -- rewrote run function to handle love and scene functions in a "clean" way
 function love.run()
-    if love.math then
-        love.math.setRandomSeed(os.time())
-    end
+	if love.load then love.load(love.arg.parseGameArguments(arg), arg) end
+ 
+	-- We don't want the first frame's dt to include time taken by love.load.
+	if love.timer then love.timer.step() end
+ 
+	local dt = 0
+ 
+	-- Main loop time.
+	return function()
+		-- Process events.
+		if love.event then
+			love.event.pump()
+			for name, a,b,c,d,e,f in love.event.poll() do
+				if name == "quit" then
+					if not love.quit or not love.quit() or not Scene.quit() then
+						return a or 0
+					end
+				end
+                love.handlers[name](a,b,c,d,e,f)
+                Scene[name](a,b,c,d,e,f) -- handle scene event, if any
+			end
+		end
+ 
+		-- Update dt, as we'll be passing it to update
+		if love.timer then dt = love.timer.step() end
+ 
+		-- Call update and draw
+		if love.update then love.update(dt) end -- will pass 0 if love.timer is disabled
+        Scene.update(dt)
 
-    if love.event then
-        love.event.pump()
-    end
-
-    if love.load then love.load(arg) end
-
-    -- We don't want the first frame's dt to include time taken by love.load.
-    if love.timer then love.timer.step() end
-
-    local dt = 0
-
-    -- Main loop time.
-    while true do
-        -- Process events.
-        if love.event then
-            love.event.pump()
-            for e,a,b,c,d in love.event.poll() do
-                if e == "quit" then
-                    if not Scene.quit() then
-                        if not love.quit or not love.quit() then
-                            if love.audio then
-                                love.audio.stop()
-                            end
-                            return
-                        end
-                    end
-                end
-                love.handlers[e](a,b,c,d)
-                Scene[e](a,b,c,d) -- handle scene event, if any
-            end
-        end
-
-        -- Update dt, as we'll be passing it to update
-        if love.timer then
-            love.timer.step()
-            dt = love.timer.getDelta()
-        end
-
-        -- Call update and draw
-        if love.update then love.update(dt) end -- will pass 0 if love.timer is disabled
-        Scene.update(dt) -- update current scene, if any
-
-        if love.window and love.graphics and love.window.isCreated() then
-            love.graphics.clear()
-            love.graphics.origin()
-            if love.draw then love.draw() end
-            Scene.draw() -- draw current scene, if any
-            love.graphics.present()
-        end
-
-        if love.timer then love.timer.sleep(0.001) end
-    end
+		if love.graphics and love.graphics.isActive() then
+			love.graphics.origin()
+			love.graphics.clear(love.graphics.getBackgroundColor())
+ 
+			if love.draw then love.draw() end
+            Scene.draw()
+			love.graphics.present()
+		end
+ 
+		if love.timer then love.timer.sleep(0.001) end
+	end
 end
